a79684ed8beabad06e5e2d26cb5c9aae96f64ec9
samadhi-spice <128258751+samadhi-spice@users.noreply.github.com>
2023-03-28 00:51:34 -0600

    Update interface.py

--- interface.py
+++ interface.py
@@ -208,7 +208,7 @@ class ModuleInterface:
             outfile.write(json_object)
 
         albums = [str(album['id']) for album in label_data['albums']['items']]
-        names = [str(name['artists.name']) for album in label_data['albums']['items']]
+        names = [str(album['artists.name']) for album in label_data['albums']['items']]
         # name='Scott Sullivan', albums=['hoak2xlwzetha', 'vc8yy71pyzqna', 'xi8jvww76oahc']
 
         return LabelInfo(
