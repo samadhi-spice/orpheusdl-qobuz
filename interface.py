7513b811ccfb4c82b27ac04b37b290960c128aa4
samadhi-spice <128258751+samadhi-spice@users.noreply.github.com>
2025-01-16 22:25:05 -0700

    revert

--- interface.py
+++ interface.py
@@ -11,9 +11,8 @@ module_information = ModuleInformation(
     service_name = 'Qobuz',
     module_supported_modes = ModuleModes.download | ModuleModes.credits,
     global_settings = {'app_id': '', 'app_secret': '', 'quality_format': '{sample_rate}kHz {bit_depth}bit'},
-    session_settings = {'username': '', 'password': '', 'userid': '', 'token': ''},
-    session_storage_variables = ['userid', 'token'],
-    login_behaviour = ManualEnum.manual,
+    session_settings = {'username': '', 'password': ''},
+    session_storage_variables = ['token'],
     netlocation_constant = 'qobuz',
     url_constants={
         'track': DownloadTypeEnum.track,
